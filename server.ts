d09b65e3069dda94fd697325b9c44ed675a9c661
Akash <AwesomeAkash47@gmail.com>
2024-06-09 12:01:55 +0530

    server connects

--- server.ts
+++ server.ts
@@ -2,18 +2,18 @@ import { WebSocketServer } from "ws";
 import express from 'express';
 
 const app = express();
-const port = 1234;
-
+const port = 6969;
+const port2 = 1234
 
 // Start the server
-const server = app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+const server = app.listen(port2, () => {
+    console.log(`Server is listening on port ${port2}`);
   });
 
 const wss = new WebSocketServer({port});
 
 
-wss.on('connection', (ws) => {
+wss.on('connection', (ws, req) => {
     
     ws.on('message', (data) => {
         console.log(`Received msg from client ${data}`);
